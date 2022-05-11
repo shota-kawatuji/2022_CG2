ae549d50fc912e38a1ce3d0a6aaf7a6905cac074
ShotaKawatsuji <k021g1403@g.neec.ac.jp>
2022-05-11 10:50:58 +0900

    新規コミット

--- main.cpp
+++ main.cpp
@@ -380,11 +380,39 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ラスタライザの設定
 	pipelineDesc.RasterizerState.CullMode = D3D12_CULL_MODE_NONE; // カリングしない
 	pipelineDesc.RasterizerState.FillMode = D3D12_FILL_MODE_SOLID; // ポリゴン内塗りつぶし
+	// pipelineDesc.RasterizerState.FillMode = D3D12_FILL_MODE_WIREFRAME; // ワイヤーフレーム
 	pipelineDesc.RasterizerState.DepthClipEnable = true; // 深度クリッピングを有効に
 
-	// ブレンドステート
-	pipelineDesc.BlendState.RenderTarget[0].RenderTargetWriteMask
-		= D3D12_COLOR_WRITE_ENABLE_ALL; // RGBA全てのチャンネルを描画
+	//// ブレンドステート
+	//pipelineDesc.BlendState.RenderTarget[0].RenderTargetWriteMask
+	//	= D3D12_COLOR_WRITE_ENABLE_ALL; // RGBA全てのチャンネルを描画
+	// レンダーターゲットのブレンド設定
+	D3D12_RENDER_TARGET_BLEND_DESC& blenddesc = pipelineDesc.BlendState.RenderTarget[0];
+	blenddesc.RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL; // RGBA全てのチャンネルを描画
+	blenddesc.BlendEnable = true; // ブレンドを有効にする
+	blenddesc.BlendOpAlpha = D3D12_BLEND_OP_ADD; // 加算
+	blenddesc.SrcBlendAlpha = D3D12_BLEND_ONE; // ソースの値を100％使う
+	blenddesc.DestBlendAlpha = D3D12_BLEND_ZERO; // テストの値を0％使う
+
+	//// 加算合成
+	//blenddesc.BlendOp = D3D12_BLEND_OP_ADD; // 加算
+	//blenddesc.SrcBlend = D3D12_BLEND_ONE; // ソースの値を100%使う
+	//blenddesc.DestBlend = D3D12_BLEND_ONE; // テストの値を100%使う
+
+	//// 減算合成
+	//blenddesc.BlendOp = D3D12_BLEND_OP_REV_SUBTRACT; // 加算
+	//blenddesc.SrcBlend = D3D12_BLEND_ONE; // ソースの値を100%使う
+	//blenddesc.DestBlend = D3D12_BLEND_ONE; // テストの値を100%使う
+
+	//// 色反転
+	//blenddesc.BlendOp = D3D12_BLEND_OP_ADD; // 加算
+	//blenddesc.SrcBlend = D3D12_BLEND_INV_DEST_COLOR; // 1.0f-テストカラーの値
+	//blenddesc.DestBlend = D3D12_BLEND_ZERO; // 使わない
+
+	// 半透明合成
+	blenddesc.BlendOp = D3D12_BLEND_OP_ADD; // 加算
+	blenddesc.SrcBlend = D3D12_BLEND_SRC_ALPHA; // ソースのアルファ値
+	blenddesc.DestBlend = D3D12_BLEND_INV_SRC_ALPHA; // 1.0f-ソースのアルファ値
 
 	//頂点レイアウトの設定
 	pipelineDesc.InputLayout.pInputElementDescs = inputLayout;
@@ -478,10 +506,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		// ４.描画コマンドここから
 		// ビューポート設定コマンド
 		D3D12_VIEWPORT viewport{};
-		viewport.Width = window_width;
+		viewport.Width = window_width / 3;
 		viewport.Height = window_height;
-		viewport.TopLeftX = 0;
-		viewport.TopLeftY = 0;
+		viewport.TopLeftX = 10;
+		viewport.TopLeftY = 10;
 		viewport.MinDepth = 0.0f;
 		viewport.MaxDepth = 1.0f;
 		// ビューポート設定コマンドを、コマンドリストに積む
@@ -489,7 +517,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		// シザー矩形
 		D3D12_RECT scissorRect{};
-		scissorRect.left = 0;									// 切り抜き座標左
+		scissorRect.left = 100;									// 切り抜き座標左
 		scissorRect.right = scissorRect.left + window_width;	// 切り抜き座標右
 		scissorRect.top = 0;									// 切り抜き座標上
 		scissorRect.bottom = scissorRect.top + window_height;	// 切り抜き座標下
